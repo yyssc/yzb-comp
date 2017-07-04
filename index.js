7c497a93217a32e022c2aa4a665e3214c6545501
Devin Chen <d3vin.chen@gmail.com>
2017-07-04 15:43:49 +0800

    fix #1 export components

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 /* eslint-disable object-shorthand */
 
 // components
-const HelloWorld = require('./lib/components/HelloWorld').default;
+const BaseSubjectRefer = require('./lib/components/BaseSubjectRefer').default;
+const FilterAccSubjectRefer = require('./lib/components/FilterAccSubjectRefer').default;
+const ReferList = require('./lib/components/ReferList').default;
+const SimpleAccSubjectChartRefer = require('./lib/components/SimpleAccSubjectChartRefer').default;
+const SimpleAccSubjectRefer = require('./lib/components/SimpleAccSubjectChartRefer').default;
+const SimpleSubjectChartRefer = require('./lib/components/SimpleAccSubjectChartRefer').default;
+const SimpleSubjectRefer = require('./lib/components/SimpleAccSubjectChartRefer').default;
 
 // shapes
 const HelloWorldShape = require('./lib/shapes/HelloWorldShape').default;
 
 module.exports = {
-  HelloWorld: HelloWorld,
+  BaseSubjectRefer: BaseSubjectRefer,
+  FilterAccSubjectRefer: FilterAccSubjectRefer,
+  ReferList: ReferList,
+  SimpleAccSubjectChartRefer: SimpleAccSubjectChartRefer,
+  SimpleAccSubjectRefer: SimpleAccSubjectRefer,
+  SimpleSubjectChartRefer: SimpleSubjectChartRefer,
+  SimpleSubjectRefer: SimpleSubjectRefer,
 
   HelloWorldShape: HelloWorldShape,
 };
